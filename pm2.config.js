817e35e882a6b0e2d7641f305303cdf055427320
Zack <mwl@live.com>
2021-06-23 18:55:47 +0800

    feat: 更新进程数

--- pm2.config.js
+++ pm2.config.js
@@ -4,7 +4,8 @@ module.exports = {
       name: 'kova',
       script: 'dist/main.js',
       exec_mode: 'cluster',
-      max_memory_restart: '500M',
+      max_memory_restart: '800M',
+      instances: Number(process.argv[2] || require('os').cpus().length),
       env: {
         NODE_ENV: 'production'
       }
