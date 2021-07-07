4c7c89201fb7e54c59e741c90f43c6da88cfa99d
Zack <mwl@live.com>
2021-07-07 14:55:00 +0800

    feat: 更改title

--- web/components/layout/index.tsx
+++ web/components/layout/index.tsx
@@ -14,7 +14,7 @@ const Layout = (props: LayoutProps) => {
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
         <meta name='theme-color' content='#000000' />
-        <title>Serverless Side Render</title>
+        <title>Kova</title>
         {/* <script dangerouslySetInnerHTML={{ __html: "var w = document.documentElement.clientWidth / 3.75;document.getElementsByTagName('html')[0].style['font-size'] = w + 'px'" }} /> */}
         { injectCss }
       </head>
